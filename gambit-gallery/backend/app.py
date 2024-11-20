a342af70c49f48065dcf9468316a297c5cd3f4d2
vm <ethan@gambit-gallery-vm.us-central1-f.c.fa24-cs411-team074-floor.internal>
2024-11-20 06:41:43 +0000

    queries working

--- gambit-gallery/backend/app.py
+++ gambit-gallery/backend/app.py
@@ -1,12 +1,87 @@
 from flask import Flask, jsonify
 from flask_cors import CORS
+import mysql.connector
 
 app = Flask(__name__)
 CORS(app)  # Enable CORS for cross-origin requests
 
+connection = mysql.connector.connect(
+    host="34.41.165.201",
+    user="root",
+    password="GambitGallery!",
+    database="gambit_gallery"
+)
+cursor = connection.cursor()
+
+queries = {
+    "UP": '''
+SELECT UserID, action, AVG(balance) AS AvgBalance, SUM(CASE WHEN balance > 0 THEN 1 ELSE 0 END) / COUNT(balance) AS WinRate
+FROM 
+    (SELECT UserID, balance, action_pre AS action
+        FROM GameHistory
+        UNION ALL
+        SELECT UserID, balance, action_flop AS action
+        FROM GameHistory
+        UNION ALL
+        SELECT UserID, balance, action_turn AS action
+        FROM GameHistory
+        UNION ALL
+        SELECT UserID,  balance, action_river AS action
+        FROM GameHistory) AS T
+GROUP BY action, UserID
+ORDER BY UserID, WinRate DESC''',
+    "CPW" : '''
+SELECT UserID, Image
+FROM (SELECT * FROM Account WHERE RoomID = 0) AS UserInRooms
+JOIN Skin ON SkinID = CurrentSkin''',
+    "AT" : '''
+SELECT g.UserID, g.Date, g.GameCount, t.NonGameCount, g.GameCount + t.NonGameCount AS TotalActivity
+FROM 
+    (SELECT UserID, DATE(DateTime) AS Date, COUNT(*) AS GameCount
+     FROM GameHistory
+     GROUP BY UserID, DATE(DateTime)) AS g
+JOIN 
+    (SELECT DISTINCT UserID, Date, COUNT(*) AS NonGameCount
+     FROM 
+((SELECT SenderID AS UserID,  DATE(DateTime) AS Date 
+FROM Transaction 
+WHERE SenderID <> 'TEXAS_HOLDEM')
+ UNION ALL 
+(SELECT ReceiverID AS UserID,  DATE(DateTime) AS Date 
+FROM Transaction 
+WHERE ReceiverID <> 'TEXAS_HOLDEM' )) AS CombinedTransactions
+    GROUP BY UserId, Date) AS t
+ON g.UserID = t.UserID AND g.Date = t.Date
+ORDER BY g.Date''',
+    'GCF' : '''
+SELECT TransactionID, SenderID, ReceiverID, Amount, CONVERT(DateTime, DATE) AS TransactionDate
+FROM Transaction
+WHERE SenderID = 'TEXAS_HOLDEM' OR ReceiverID = 'TEXAS_HOLDEM'
+GROUP BY TransactionDate, TransactionID, SenderID, ReceiverID, Amount''',
+    'RS' : '''
+SELECT 
+    UserID,
+    SUM(CASE WHEN balance > 0 THEN 1 ELSE 0 END) AS Positive,
+    SUM(CASE WHEN balance < 0 THEN 1 ELSE 0 END) AS Negative,
+    SUM(CASE WHEN balance = 0 THEN 1 ELSE 0 END) AS Zero
+FROM GameHistory
+WHERE  handID IN ( SELECT handID 
+FROM GameHistory g 
+WHERE g.UserID LIKE "00021ae5")
+GROUP BY UserID''',
+}
+
 @app.route('/api/hello', methods=['GET'])
 def hello():
     return jsonify({"message": "Hello from Flask!"})
+
+
+@app.route('/api/query/<q>', methods=['GET'])
+def query(q):
+    cursor.execute(queries[q])
+    return jsonify({"query": cursor.fetchall()})
+
+
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
