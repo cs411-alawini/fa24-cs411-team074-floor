59d15a507f25479048b9fa9b49f311e6c26666b4
danielcao888 <dscao2@illinois.edu>
2024-12-08 17:43:41 -0600

    Finished Basic User Stats Page

--- gambit-gallery/frontend/src/app/data-page/data-page.component.ts
+++ gambit-gallery/frontend/src/app/data-page/data-page.component.ts
@@ -7,9 +7,6 @@ import { MatSortModule } from '@angular/material/sort';
 import { MatSort } from '@angular/material/sort';
 import { MatTableDataSource } from '@angular/material/table';
 
-
-import { TestService } from './test.service';
-
 @Component({
   selector: 'app-data-page',
   standalone: true,
@@ -17,63 +14,22 @@ import { TestService } from './test.service';
   templateUrl: './data-page.component.html',
   styleUrls: ['./data-page.component.css'],
 })
-export class DataPageComponent implements OnInit {
-  displayedColumns: string[] = ['column1', 'column2'];
-  dataSource = new MatTableDataSource<any>([]);
-  rooms: any = null;
+export class DataPageComponent {
+constructor(private location: Location) {}
+  dataSourceMoves = [
+    { action: 'Raise', balance: '+200', winRate: '75%' },
+    { action: 'Call', balance: '-50', winRate: '40%' },
+  ];
+  movesColumns: string[] = ['action', 'balance', 'winRate'];
 
+  dataSourceHistory = [
+    { user: 'User1', pos: 5, neg: 3, zero: 2 },
+    { user: 'User2', pos: 10, neg: 8, zero: 0 },
+  ];
+  historyColumns: string[] = ['user', 'positive', 'negative', 'zero'];
 
-  @ViewChild(MatPaginator) paginator!: MatPaginator;
-  @ViewChild(MatSort) sort!: MatSort;
-
-  constructor(private location: Location, private buttonService: TestService) {}
-
-  ngOnInit() {
-    const mockData = [
-      { column1: 'Data 1', column2: 'Data 2' },
-      { column1: 'Data 3', column2: 'Data 4' },
-    ];
-    this.dataSource.data = mockData;
-  }
-  ngAfterViewInit() {
-    this.dataSource.paginator = this.paginator;
-    this.dataSource.sort = this.sort;
-  }
   goBack(): void {
     this.location.back();
-  }
-
-  getRooms(response: any): void {
-    this.rooms = response.result
-    // for (let r in response.result) {
-    //   this.rooms += r.toString()
-    // }
-    console.log(this.rooms)
-  }
-
-  onButton1Click(): void {
-    this.buttonService.button1Action().subscribe(
-      (response) => console.log(response.message),
-      (error) => console.error('Error:', error)
-    );
-
-    this.buttonService.getRooms().subscribe(
-      (response) => this.getRooms(response),
-      (error) => console.error('Error:', error)
-    );
-  
-  }
-
-  onButton2Click(): void {
-    this.buttonService.button2Action().subscribe(
-      (response) => console.log(response.message),
-      (error) => console.error('Error:', error)
-    );
-
-    this.buttonService.getRooms().subscribe(
-      (response) => this.getRooms(response),
-      (error) => console.error('Error:', error)
-    );
   }
 
 }
