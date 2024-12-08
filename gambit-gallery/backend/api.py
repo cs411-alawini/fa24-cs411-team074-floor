ffa68ce3fe6e60243c7f1d85f4d6a1b9113f45e3
Ethan <ethanytchang@gmail.com>
2024-12-08 16:48:51 -0600

    ready to merge into main

---
+++ gambit-gallery/backend/api.py
@@ -0,0 +1,125 @@
+from flask import Flask, jsonify
+from flask_cors import CORS
+from mysql.connector import connection
+
+app = Flask(__name__)
+CORS(app, resources={r"/api/*": {"origins": "http://localhost:4200"}})
+
+connection = connection.MySQLConnection(
+    user="root", password="root", database="gambit_gallery"
+)
+# connection = mysql.connector.connect(
+#     host="34.41.165.201",
+#     user="root",
+#     password="GambitGallery!",
+#     database="gambit_gallery",
+#     connection_timeout = 10
+# )
+cursor = connection.cursor()
+
+queries = {
+    "UP": '''
+SELECT UserID, action, AVG(balance) AS AvgBalance, SUM(CASE WHEN balance > 0 THEN 1 ELSE 0 END) / COUNT(balance) AS WinRate
+FROM 
+    (SELECT UserID, balance, action_pre AS action
+        FROM GameHistory
+        UNION ALL
+        SELECT UserID, balance, action_flop AS action
+        FROM GameHistory
+        UNION ALL
+        SELECT UserID, balance, action_turn AS action
+        FROM GameHistory
+        UNION ALL
+        SELECT UserID,  balance, action_river AS action
+        FROM GameHistory) AS T
+GROUP BY action, UserID
+ORDER BY UserID, WinRate DESC''',
+    "CPW" : '''
+SELECT UserID, Image
+FROM (SELECT * FROM Account WHERE RoomID = 0) AS UserInRooms
+JOIN Skin ON SkinID = CurrentSkin''',
+    "AT" : '''
+SELECT g.UserID, g.Date, g.GameCount, t.NonGameCount, g.GameCount + t.NonGameCount AS TotalActivity
+FROM 
+    (SELECT UserID, DATE(DateTime) AS Date, COUNT(*) AS GameCount
+     FROM GameHistory
+     GROUP BY UserID, DATE(DateTime)) AS g
+JOIN 
+    (SELECT DISTINCT UserID, Date, COUNT(*) AS NonGameCount
+     FROM 
+((SELECT SenderID AS UserID,  DATE(DateTime) AS Date 
+FROM Transaction 
+WHERE SenderID <> 'TEXAS_HOLDEM')
+ UNION ALL 
+(SELECT ReceiverID AS UserID,  DATE(DateTime) AS Date 
+FROM Transaction 
+WHERE ReceiverID <> 'TEXAS_HOLDEM' )) AS CombinedTransactions
+    GROUP BY UserId, Date) AS t
+ON g.UserID = t.UserID AND g.Date = t.Date
+ORDER BY g.Date''',
+    'GCF' : '''
+SELECT TransactionID, SenderID, ReceiverID, Amount, CONVERT(DateTime, DATE) AS TransactionDate
+FROM Transaction
+WHERE SenderID = 'TEXAS_HOLDEM' OR ReceiverID = 'TEXAS_HOLDEM'
+GROUP BY TransactionDate, TransactionID, SenderID, ReceiverID, Amount''',
+    'RS' : '''
+SELECT 
+    UserID,
+    SUM(CASE WHEN balance > 0 THEN 1 ELSE 0 END) AS Positive,
+    SUM(CASE WHEN balance < 0 THEN 1 ELSE 0 END) AS Negative,
+    SUM(CASE WHEN balance = 0 THEN 1 ELSE 0 END) AS Zero
+FROM GameHistory
+WHERE  handID IN ( SELECT handID 
+FROM GameHistory g 
+WHERE g.UserID LIKE "00021ae5")
+GROUP BY UserID''',
+}
+
+@app.route('/api/hello', methods=['GET'])
+def hello():
+    return jsonify({"message": "Hello from Flask!"})
+
+@app.route('/api/query/<q>', methods=['GET'])
+def query(q):
+    cursor.execute(queries[q])
+    return jsonify({"query": cursor.fetchall()})
+
+@app.route('/api/query/get_skins')
+def get_skins():
+    cursor.execute('select * from Skin;')
+    return jsonify({"result": cursor.fetchall()})
+
+@app.route('/api/get_rooms', methods=['GET'])
+def get_rooms():
+    cursor.execute('select * from Room')
+    return jsonify({"result": cursor.fetchall()})
+
+@app.route('/api/create_room', methods=['GET'])
+def create_room():
+    cursor.execute('insert into Room values ("test", "nah", "ur mom") on duplicate key update RoomId = RoomId;')
+    return jsonify({"result": cursor.fetchall()})
+
+@app.route('/api/join_room', methods=['GET'])
+def join_room():
+    status = ""
+    
+    cursor.execute('insert into Room values ("test", "nah", "ur mom") on duplicate key update RoomId = RoomId;')
+    return jsonify({"result": cursor.fetchall()})
+
+@app.route('/api/delete_room', methods=['GET'])
+def delete_room():
+    cursor.execute('delete from Room where RoomId = "test";')
+    return jsonify({"result": cursor.fetchall()})
+
+@app.route('/api/get_log', methods=['GET'])
+def get_log():
+    cursor.execute('select ChatLog from Room where RoomId = "test";')
+    return jsonify({"result": cursor.fetchall()})
+
+@app.route('/api/update_log/<room>/<text>', methods=['GET'])
+def update_log(room, text):
+    cursor.execute(f'update Room set ChatLog = "{text}" where RoomId = "{room}";')
+    return jsonify({"result": cursor.fetchall()})
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5000)
