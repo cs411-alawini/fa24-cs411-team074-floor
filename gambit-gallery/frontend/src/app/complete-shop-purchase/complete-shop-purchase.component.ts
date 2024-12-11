677206beddc950d3f3f1bdb93194aa599a03ebfe
Ethan <ethanytchang@gmail.com>
2024-12-11 15:38:16 -0600

    shop transaction works

--- gambit-gallery/frontend/src/app/complete-shop-purchase/complete-shop-purchase.component.ts
+++ gambit-gallery/frontend/src/app/complete-shop-purchase/complete-shop-purchase.component.ts
@@ -1,20 +1,29 @@
+import { UserService } from './../services/user.service';
 import { Component, OnInit } from '@angular/core';
-import { RouterModule, Router  } from '@angular/router';
+import { RouterModule, Router } from '@angular/router';
 import { Location, CommonModule } from '@angular/common';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { CartService } from '../cart.service';
+import { ApiService } from '../services/api.service';
 
 @Component({
   selector: 'app-complete-shop-purchase',
   standalone: true,
-  imports: [HttpClientModule, RouterModule,CommonModule],
+  imports: [HttpClientModule, RouterModule, CommonModule],
   templateUrl: './complete-shop-purchase.component.html',
-  styleUrls: ['./complete-shop-purchase.component.css']
+  styleUrls: ['./complete-shop-purchase.component.css'],
 })
-export class CompleteShopPurchaseComponent implements OnInit{
+export class CompleteShopPurchaseComponent implements OnInit {
   cart: any[] = [];
 
-  constructor(private router: Router, private location: Location, private http: HttpClient, private cartService: CartService) {}
+  constructor(
+    private api: ApiService,
+    private userService: UserService,
+    private router: Router,
+    private location: Location,
+    private http: HttpClient,
+    private cartService: CartService
+  ) {}
 
   ngOnInit() {
     this.cart = this.cartService.getCart();
@@ -25,7 +34,16 @@ export class CompleteShopPurchaseComponent implements OnInit{
   }
 
   completePurchase() {
+    let items = '';
+    this.cart.forEach((c) => {
+      items += ' ' + c.Description;
+    });
+    this.api.sendFunds(
+      this.userService.getUsername(),
+      'SHOP',
+      this.cart.length,
+      'Purchased: ' + items
+    );
     this.router.navigate(['/home']);
   }
-
 }
